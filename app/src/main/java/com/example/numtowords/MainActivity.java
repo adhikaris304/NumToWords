3a2c75f009ae80a539812f8b00757347b0017311
Suman Adhikari <adhikaris304@gmail.com>
2019-04-11 20:09:33 +0545

    Final one

--- app/src/main/java/com/example/numtowords/MainActivity.java
+++ app/src/main/java/com/example/numtowords/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.numtowords;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etnumber;
+       private TextView result;
+      private Button btnconvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etnumber = findViewById(R.id.etnumber);
+        result = findViewById(R.id.result);
+        btnconvert = findViewById(R.id.btnconvert);
+
+        btnconvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number =Integer.parseInt(etnumber.getText().toString());
+                Words c=new Words();
+                String word=c.conversion(number);
+                result.setText(word);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/numtowords/Words.java
+++ app/src/main/java/com/example/numtowords/Words.java
@@ -1,4 +1,32 @@
 package com.example.numtowords;
 
 public class Words {
+    public String word;
+    String[] ones = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+    String[] tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+public Words(){
+
+}
+public String getWord(){
+    return word;
+}
+
+public void setWord(){
+    this.word=word;
+}
+public String conversion(int number){
+    if (number % 100 < 20) {
+        word=ones[number%20];
+        number /=100;
+    }else {
+        word=ones[number%10];
+        number /=10;
+        word = tens[number % 10] +" "+ word;
+        number /= 10;
+    }
+    if (number == 0)
+        return word;
+    return ones[number] +" " + " hundred " + " and " + word;
+}
 }
